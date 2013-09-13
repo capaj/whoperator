a033d480651707a134951c8e75d4750d5b697e23
cohena <cohena@gmail.com>
2013-09-12 21:07:23 -0700

    Fix updated time on scan page

--- whoperator/static/js/app/scan/scan.js
+++ whoperator/static/js/app/scan/scan.js
@@ -2,26 +2,55 @@
     'use strict';
     var app = angular.module('whoperator');
 
-    app.controller('ScanCtrl', ['ScanService', function(ScanService) {
-        this.tableTitles = ['Status', 'Torrent ID', 'Torrent', 'Info Hash', 'Updated Time'];
-        this.scanItems = ScanService.get();
+    app.controller('ScanCtrl', ['TorrentFileCollectionService', 'TorrentFileService',
+        function(TorrentFileCollectionService, TorrentFileService) {
+            this.tableTitles = ['Status', 'Torrent ID', 'Torrent', 'Info Hash', 'Updated Time'];
+            this.torrentFileCollections = TorrentFileCollectionService.get();
+            this.torrentFileItems = TorrentFileService.get();
     }]);
 
-    app.factory('ScanService', ['$http', function($http) {
+    app.factory('TorrentFileCollectionService', ['$http', function($http) {
         return {
           get: function() {
-            return $http.get('/torrent_collection/1/item').then(function(result) {
-                return _.map(result.data.items, function(item) {
-                    return new ScanItem(_.extend(item, { updated: result.data.items.updated }));
+            return $http.get('/torrent_collection/1').then(function(result) {
+                var keys = [];
+                for (var key in result.data) {
+                  if (result.data.hasOwnProperty(key)) {
+                    keys.push(key);
+                  }
+                }
+
+                return keys.map(function(key) {
+                    return new TorrentFileCollectionItem(result.data[key]);
                 });
             });
           }
         }
     }]);
 
-    function ScanItem(data) {
+    app.factory('TorrentFileService', ['$http', function($http) {
+        return {
+          get: function() {
+            return $http.get('/torrent_collection/1/item').then(function(result) {
+                return _.map(result.data.items, function(item) {
+                    return new TorrentFileItem(_.extend(item, { updated: result.data.items.updated }));
+                });
+            });
+          }
+        }
+    }]);
+
+    function TorrentFileCollectionItem(data) {
+        this.id = data.id;
+        this.name = data.name;
+        this.path = data.path;
+        this.recurse = data.recurse;
+        this.updated = data.updated;
+    }
+
+    function TorrentFileItem(data) {
         this.torrentId = data.torrent_id;
-        // mark as error if collection_id is -1
+        // mark as error if torrent_id is -1
         this.error = !~data.torrent_id;
         this.status = this.error ? 'invalid' : 'verified';
         this.torrent = data.rel_path;
