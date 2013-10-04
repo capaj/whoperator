fc4b24715afd03f9d1b0fcbb763b31fcf2422dcf
cohena <cohena@gmail.com>
2013-10-03 23:54:32 -0700

    Continued filescanner restructuring

--- whoperator/filescanner.py
+++ whoperator/filescanner.py
@@ -11,6 +11,7 @@ import uuid
 # Processing callbacks handle specific file types and return usable data.
 # Data callbacks do something with the data returned by the processing callback.
 
+
 class InvalidScanTargetException(Exception):
     pass
 
@@ -20,12 +21,15 @@ class FileScanner(object):
         self.scanning_queue = deque([])
         self.global_filetype_handlers = {}
         self.scan_thread = None
-        self.current_scan = None
+        self.current_job = None
         self.stop_event = Event()
         self.app = app
 
     def set_global_filetype_handler(self, type_glob_string, handler_callback):
-        self.global_filetype_handlers[type_glob_string] = handler_callback
+        if callable(handler_callback):
+            self.global_filetype_handlers[type_glob_string] = handler_callback
+        else:
+            raise Exception("Callback for filetype handler wasn't callable.")
 
     def remove_global_filetype_handler(self, type_glob_string):
         if type_glob_string in self.global_filetype_handlers:
@@ -42,12 +46,14 @@ class FileScanner(object):
             raise InvalidScanTargetException("No filetype handler defined for current filetype_filter (%s)." %
                                              filetype_filter)
 
-        queue_item = ScannerJob(directory_path, file_data_callback, context, filetype_filter, recurse)
-        self.app.logger.info("New scan job id: %s" % queue_item.job_id)
+        job = ScannerJob(directory_path, file_data_callback, None, context, filetype_filter, recurse)
+        job.filetype_handlers = self.global_filetype_handlers.copy()
+
+        self.app.logger.info("New scan job id: %s" % job.job_id)
         if priority:
-            self.scanning_queue.appendleft(queue_item)
+            self.scanning_queue.appendleft(job)
         else:
-            self.scanning_queue.append(queue_item)
+            self.scanning_queue.append(job)
 
         self._start_scan_thread()
 
@@ -58,50 +64,35 @@ class FileScanner(object):
         if not len([fnmatch.fnmatch(file_path, pattern) for pattern in self.global_filetype_handlers.iterkeys()]):
             raise InvalidScanTargetException("No filetype_handler defined for specified file.")
 
-        queue_item = ScannerJob(file_path, file_data_callback, context, '*', False)
-        self.app.logger.info("New scan job id: %s" % queue_item.job_id)
+        job = ScannerJob(file_path, file_data_callback, None, context, '*', False)
+        job.filetype_handlers = self.global_filetype_handlers.copy()
+
+        self.app.logger.info("New scan job id: %s" % job.job_id)
         if priority:
-            self.scanning_queue.appendleft(queue_item)
+            self.scanning_queue.appendleft(job)
         else:
-            self.scanning_queue.append(queue_item)
+            self.scanning_queue.append(job)
 
         self._start_scan_thread()
 
     def _process_scan_queue(self, scanning_queue, stop_event):
         while len(scanning_queue) and not stop_event.is_set():
-            self.current_scan = scanning_queue.popleft()
+            self.current_job = scanning_queue.popleft()
 
-            path, data_callback, context, filetype_filter, recurse = self.current_scan
-            if os.path.isdir(path):
-                if recurse:
-                    for root_dir, dirnames, filenames in os.walk(path):
-                        if filetype_filter != '*':
-                            filenames = fnmatch.filter(filenames, filetype_filter)
-                        for filename in filenames:
-                            scanning_queue.appendleft((os.path.join(root_dir, filename), data_callback, context, filetype_filter, False))
+            self.app.logger.debug("Starting scan job id: %s" % self.current_job.job_id)
+
+            for file_to_scan in self.current_job.files():
+                if not stop_event.is_set():
+                    self.app.logger.debug("Scanning file: %s" % file_to_scan.path)
+                    file_to_scan.scan()
                 else:
-                    file_paths = os.listdir(path)
-                    if filetype_filter != '*':
-                        file_paths = fnmatch.filter(file_paths, filetype_filter)
-                    for file_path in file_paths:
-                        scanning_queue.appendleft((os.path.join(path, file_path), data_callback, context, filetype_filter, False))
-                self.current_scan = None
-                continue
+                    scanning_queue.appendleft(self.current_job)
+                    self.current_job = None
+                    return
 
-            processing_callbacks = [callback for type_glob_string, callback
-                                    in self.global_filetype_handlers.iteritems()
-                                    if fnmatch.fnmatch(path, type_glob_string)]
+            self.current_job.complete()
 
-            for processing_callback in processing_callbacks:
-                self.app.logger.debug("Scanning file: %s" % path)
-                processing_result = processing_callback(path, context=context)
-                if processing_result is not None and data_callback is not None:
-                    if isinstance(processing_result, (list, tuple)):
-                        data_callback(*processing_result, context=context)
-                    else:
-                        data_callback(processing_result, context=context)
-
-            self.current_scan = None
+            self.current_job = None
 
     def _start_scan_thread(self):
         if not self.scan_thread:
@@ -125,11 +116,11 @@ class FileScanner(object):
 
     def clear_queue(self):
         self.scanning_queue.clear()
-        self.current_scan = None
+        self.current_job = None
 
 
 class ScannerJob():
-    def __init__(self, path, file_data_callback, context, filetype_filter, recurse):
+    def __init__(self, path, file_data_callback, completion_callback, context, filetype_filter, recurse):
         self.job_id = uuid.uuid4()
         self.filetype_handlers = {}
 
@@ -137,7 +128,7 @@ class ScannerJob():
         self.is_dir = os.path.isdir(self.scan_path)
 
         self.data_callback = file_data_callback
-        self.completion_callback = None
+        self.completion_callback = completion_callback
 
         self.context = context
         self.filetype_filter = filetype_filter
@@ -150,31 +141,31 @@ class ScannerJob():
                     if self.filetype_filter != '*':
                         filenames = fnmatch.filter(filenames, self.filetype_filter)
                     for filename in filenames:
-                        yield ScannerJobFile(os.path.join(root_dir, filename), self.data_callback, self.context)
+                        processing_callbacks = [callback for type_glob_string, callback
+                                in self.filetype_handlers.iteritems()
+                                if fnmatch.fnmatch(filename, type_glob_string)]
+                        yield ScannerJobFile(os.path.join(root_dir, filename), processing_callbacks, self.data_callback, self.context)
             else:
                 file_paths = os.listdir(self.scan_path)
                 if self.filetype_filter != '*':
                     file_paths = fnmatch.filter(file_paths, self.filetype_filter)
                 for file_path in file_paths:
-                    yield ScannerJobFile(os.path.join(self.scan_path, file_path), self.data_callback, self.context)
+                    processing_callbacks = [callback for type_glob_string, callback
+                                in self.filetype_handlers.iteritems()
+                                if fnmatch.fnmatch(file_path, type_glob_string)]
+                    yield ScannerJobFile(os.path.join(self.scan_path, file_path), processing_callbacks, self.data_callback, self.context)
         else:  # single file
             yield ScannerJobFile(self.scan_path, self.data_callback, self.context)
 
-        #processing_callbacks = [callback for type_glob_string, callback
-        #                        in self.filetype_handlers.iteritems()
-        #                        if fnmatch.fnmatch(self.path, type_glob_string)]
-        #
-        #for processing_callback in processing_callbacks:
-        #    self.app.logger.debug("Scanning file: %s" % self.path)
-        #    processing_result = processing_callback(path, context=context)
-        #    if processing_result is not None and data_callback is not None:
-        #        if isinstance(processing_result, (list, tuple)):
-        #            data_callback(*processing_result, context=context)
-        #        else:
-        #            data_callback(processing_result, context=context)
+    def complete(self):
+        if callable(self.completion_callback):
+            self.completion_callback(self.job_id, self.scan_path, self.context)
 
     def set_filetype_handler(self, type_glob_string, handler_callback):
-        self.filetype_handlers[type_glob_string] = handler_callback
+        if callable(handler_callback):
+            self.filetype_handlers[type_glob_string] = handler_callback
+        else:
+            raise Exception("Callback for filetype handler wasn't callable.")
 
     def remove_filetype_handler(self, type_glob_string):
         if type_glob_string in self.filetype_handlers:
@@ -185,8 +176,20 @@ class ScannerJob():
 
 
 class ScannerJobFile():
-    def __init__(self, path, file_data_callback, context):
+    def __init__(self, path, processing_callbacks, file_data_callback, context):
         self.path = path
+        self.processing_callbacks = list(processing_callbacks) if processing_callbacks else []
         self.data_callback = file_data_callback
         self.context = context
 
+    def scan(self):
+        for processing_callback in self.processing_callbacks:
+            if not callable(processing_callback):
+                raise Exception("Processing callback was uncallable.")
+
+            processing_result = processing_callback(self.path, context=self.context)
+            if processing_result is not None and self.data_callback is not None:
+                if isinstance(processing_result, (list, tuple)):
+                    self.data_callback(*processing_result, context=self.context)
+                else:
+                    self.data_callback(processing_result, context=self.context)
